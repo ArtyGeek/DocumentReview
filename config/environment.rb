b81a5ab574362ba523a905921f657c25e5d4f3f3
YurkoHoshko <yurko.krain@gmail.com>
2013-09-11 17:04:19 +0300

    mailer configuration #2

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Documentreview::Application.initialize!
 ActionMailer::Base.default_url_options = { :host => 'artygeek-rails-demo1.heroku.com' }
 ActionMailer::Base.default from: 'rails-demo@artygeek.com'
 
-ActionMailer.smtp_settings = {
+ActionMailer::Base.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: ENV["DOMAIN_NAME"],
